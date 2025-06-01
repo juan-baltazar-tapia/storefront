e44fc5fffd20a6b8d7d71f632ed8432803a120d5
MrFxde <juan.space14@gmail.com>
2025-05-31 23:27:24 -0400

    Many to Many relationship

--- store/models.py
+++ store/models.py
@@ -1,12 +1,26 @@
 from django.db import models
 
 #have it inhereit the models class in Django.
+class Promotion(models.Model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+    #products
+
+#Circular Dependancy Problem
+class Collection(models.Model):
+    title = models.CharField(max_length=255)
+    # Wrap product in quotes to solve circular dependacy problem
+    featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
+
 class Product(models.Model):
     title = models.CharField(max_length=255) 
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -37,6 +51,18 @@ class Order(models.Model):
         (PAYMENT_STATUS_FAILED, 'Failed')
     ]
 
-    place_at = models.DateTIMEField(auto_now_add=True)
+    place_at = models.DateTimeField(auto_now_add=True)
     payment_status = models.CharField(max_length=1, choices=PAYMENT_STATUS, default=PAYMENT_STATUS_PENDING)
-    
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+ #one to many relationship wtih Address and Customer   
+class Address(models.Model):
+    street = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    # this is a child entity, and you need the parent in ForeignKey()
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE)

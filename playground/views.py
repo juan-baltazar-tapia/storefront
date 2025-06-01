2ef9425adb806cea1ebfa784280c7af267e021af
MrFxde <juan.space14@gmail.com>
2025-05-31 21:38:25 -0400

    Added Data Models

--- playground/views.py
+++ playground/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here. AKA request handler
 def say_hello(request):
+    x = 2
+    y = 2
     return render(request, 'hello.html', { 'name': 'Juan'})
